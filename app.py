99d31569d0a40c486a9cd3ad8fe7f721fd1763fa
vunh2301 <vunh2301@gmail.com>
2023-04-09 14:10:12 +0700

    clear comment

--- app.py
+++ app.py
@@ -9,24 +9,3 @@ engine = create_engine(db_connection_string,
                        }},
                        echo=True)
 conn = engine.connect()
-
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
-# from models import User
-
-# students = Table(
-#   'students',
-#   meta,
-#   Column('id', Integer, primary_key=True),
-#   Column('name', String),
-#   Column('gender', String),
-#   Column('brithday', String),
-#   Column('address', String),
-# )
-# s = students.select()
-
-# result = conn.execute(s)
-# for row in result:
-#   print(row)
-# db = SQLAlchemy(app)
-# login = LoginManager(app)
